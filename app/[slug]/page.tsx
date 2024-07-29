de6e99781583c690b0ef9bf1266b1f2c599c5d47
Aaron Smulktis <aaronsmulktis@gmail.com>
2024-07-29 16:16:00 -0400

    tweaks

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -12,7 +12,7 @@ import { notFound } from 'next/navigation'
 async function fetchData(slug: string) {
   // If the slug is 'blog', fetch all posts.
   if (slug === 'blog') {
-    return {posts: await getAllPosts(), context: 'blog'}
+    return { posts: await getAllPosts(), context: 'blog' }
   }
 
   // If the slug is 'books', fetch all books.
@@ -25,22 +25,22 @@ async function fetchData(slug: string) {
 
   // If page data exists, return it.
   if (page) {
-    return {post: page}
+    return { post: page }
   }
 
   // Otherwise, return an error.
-  return {error: 'No data found'}
+  return { error: 'No data found' }
 }
 
 /**
  * Render a single page.
  */
-function RenderPage({page}: {page: Page}) {
+function RenderPage({ page }: { page: Page }) {
   return (
     <main className="flex flex-col gap-8">
       <article>
-        <h1 dangerouslySetInnerHTML={{__html: page.title}} />
-        <div dangerouslySetInnerHTML={{__html: page.content}} />
+        <h1 dangerouslySetInnerHTML={{ __html: page.title }} />
+        <div dangerouslySetInnerHTML={{ __html: page.content }} />
       </article>
     </main>
   )
@@ -49,7 +49,7 @@ function RenderPage({page}: {page: Page}) {
 /**
  * Render posts list.
  */
-function RenderPostsList({posts, context}: {posts: Post[]; context: string}) {
+function RenderPostsList({ posts, context }: { posts: Post[]; context: string }) {
   return (
     <main className="flex flex-col gap-8">
       <h1 className="capitalize">Latest {context}</h1>
@@ -64,12 +64,12 @@ function RenderPostsList({posts, context}: {posts: Post[]; context: string}) {
               priority={true}
             />
             <Link href={`/${context}/${post.slug}`}>
-              <h2 dangerouslySetInnerHTML={{__html: post.title}} />
+              <h2 dangerouslySetInnerHTML={{ __html: post.title }} />
             </Link>
             <p className="text-sm text-gray-500">
               {post.commentCount} Comments
             </p>
-            <div dangerouslySetInnerHTML={{__html: post.excerpt}} />
+            <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
             <Link className="button" href={`/${context}/${post.slug}`}>
               View Post
             </Link>
@@ -83,9 +83,9 @@ function RenderPostsList({posts, context}: {posts: Post[]; context: string}) {
 /**
  * Catch-all Archive Page route.
  */
-export default async function Archive({params}: {params: {slug: string}}) {
+export default async function Archive({ params }: { params: { slug: string } }) {
   // Get the slug from the params.
-  const {slug} = params
+  const { slug } = params
 
   // Fetch data from WordPress.
   const data = await fetchData(slug)
